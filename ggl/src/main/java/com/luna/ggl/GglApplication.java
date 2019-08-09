ae1de11c3ae9982e4f061a4863ea5c78d1308410
Administrator <1658353651@qq.com>
2019-08-09 09:31:59 +0800

    1

---
+++ ggl/src/main/java/com/luna/ggl/GglApplication.java
@@ -0,0 +1,13 @@
+package com.luna.ggl;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GglApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GglApplication.class, args);
+    }
+
+}
